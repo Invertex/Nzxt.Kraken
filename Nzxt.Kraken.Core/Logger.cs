ca6aba01b7ef07be70821cb7f9d69fb3ee025d28
aidan <aidan.gilbert@live.co.uk>
2018-04-29 00:02:42 +0100

    Ignore ACK failure when device is power cycled.
     Adds some logic to read/set the device id although
     it isn't used for anything.

---
+++ Nzxt.Kraken.Core/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nzxt.Kraken.Core
+{
+    public static class Logger
+    {
+        public static void Write(string message)
+        {
+#if DEBUG
+            Console.WriteLine(message);
+#endif
+        }
+    }
+}
--- Nzxt.Kraken.Core/Manager.cs
+++ Nzxt.Kraken.Core/Manager.cs
@@ -5,6 +5,10 @@ namespace Nzxt.Kraken.Core
 {
     public class Manager
     {
+        public static byte DEVICE_ACK = 15;
+
+        public static byte DEVICE_NUMBER = 10;
+
         public static readonly byte[] LightingChannels = new byte[]
         {
             1,
@@ -34,23 +38,37 @@ namespace Nzxt.Kraken.Core
                 1, //Unknown
                 89 //Unknown
             };
-            this.Device.Write(data, false);
+            if (!this.Device.Write(data, false))
+            {
+                throw ManagerException.Initialize;
+            }
             Thread.Sleep(1000); //Unknown
-            this.Read(true);
+            if (!this.Read(true))
+            {
+                //This happens after power cycle. 
+                //I have no idea why.
+                //Seems you can ignore it?
+            }
             this.GetOrAddDeviceId();
         }
 
         protected virtual void GetOrAddDeviceId()
         {
-            this.Id = this.Data[10]; //Device number.
-            if (this.Data[0] == 4 && this.Data[15] != 31) //Unknown
+            this.Id = this.Data[DEVICE_NUMBER]; //Device number.
+            if (this.Data[0] == 4 && this.Data[DEVICE_ACK] != 31) //Unknown
             {
                 if (this.Id == 0 || this.Id == byte.MaxValue)
                 {
                     //Perhaps this happens for new devices?
+                    //I've never been here.
                     this.AddDeviceId();
                     //We might need to restart the driver here.
                 }
+            }
+            else
+            {
+                //I've never been here either.
+                this.ConfirmDeviceId();
             }
         }
 
@@ -60,31 +78,69 @@ namespace Nzxt.Kraken.Core
             data[0] = 2; //Unknown
             data[1] = 70; //Unknown
             data[2] = this.CreateDeviceId();
-            this.Device.Write(data, false);
+            if (!this.Device.Write(data, false))
+            {
+                throw ManagerException.Identify;
+            }
         }
 
         protected virtual byte CreateDeviceId()
         {
+            //I don't know what the fuck this is all about.
+            //while (!success) { randomize(); }
             return (byte)new Random(
                 unchecked((int)DateTime.Now.Ticks)
             ).Next(1, byte.MaxValue);
+        }
+
+        protected virtual void ConfirmDeviceId()
+        {
+            var data = new byte[65];
+            data[0] = 16; //Unknown
+            data[1] = 1; //Unknown
+            if (!this.Device.Write(data, false))
+            {
+                //Is this an error? It appears to be ignored.
+                return;
+            }
+            if (!this.Device.Read(this.Data, false))
+            {
+                throw ManagerException.Identify;
+            }
+            if (this.Data[4] == 2)  //Unknown
+            {
+                //Some reference to "IsFWBoots = True".
+                //I don't know what it means but the value does not seem to be used.
+                //It may be something to do with firmware updates.
+            }
+            if (!this.Device.Read(this.Data, false))
+            {
+                throw ManagerException.Identify;
+            }
+            if (this.Id != this.Data[DEVICE_NUMBER])
+            {
+                throw ManagerException.Identify;
+            }
         }
 
         public bool Read(bool required)
         {
             if (required)
             {
-                var a = 0;
-                while (!this.Read(false) || this.Data[15] == 0)  //ACK.
+                for (var a = 0; a <= 10; a++)
                 {
-                    Thread.Sleep(5);
-                    if (a++ >= 10)
+                    if (this.Read(false) && this.Data[DEVICE_ACK] != 0)
                     {
-                        throw new InvalidOperationException("Device failed to respond.");
+                        return true;
                     }
+                    Thread.Sleep(5);
                 }
+                return false;
             }
-            return this.Device.Read(this.Data, false);
+            else
+            {
+                return this.Device.Read(this.Data, false);
+            }
         }
 
         public int Temperature
@@ -153,6 +209,30 @@ namespace Nzxt.Kraken.Core
             data[4] = percent; //Fan/Pump percent. 
             this.Device.Write(data, false);
             Thread.Sleep(1000); //Unknown
+        }
+    }
+
+    public class ManagerException : Exception
+    {
+        public ManagerException(string message) : base(message)
+        {
+
+        }
+
+        public static Exception Initialize
+        {
+            get
+            {
+                return new ManagerException("Failed to initialize device.");
+            }
+        }
+
+        public static Exception Identify
+        {
+            get
+            {
+                return new ManagerException("Failed to determine or create device identifier.");
+            }
         }
     }
 }
